9ed48cb31e16e7b552f9490219c546a3d2c59834
gbkabel <112282802+gbkabel@users.noreply.github.com>
2022-11-22 15:17:50 -0800

    fixed sprite

--- Player.cpp
+++ Player.cpp
@@ -1,35 +1,9 @@
-#include "player.h"
+#include "Player.h"
 
 Player::Player()
 {
-	speed = 250;
-	health = 100;
-	playerTexture.loadFromFile("graphics/shooter1.png");
+	playerTexture.loadFromFile("shooter1.png");
 	playerSprite.setTexture(playerTexture);
-	playerSprite.setOrigin(25, 25);
-}
-
-void Player::spawn(Vector2f resolution)
-{
-	mousePosition.x = resolution.x / 2;
-	mousePosition.y = resolution.y / 2;
-	resolution.x = resolution.x;
-	resolution.y = resolution.y;
-}
-
-FloatRect Player::getPosition()
-{
-	return playerSprite.getGlobalBounds();
-}
-
-Vector2f Player::getCenter()
-{
-	return mousePosition;
-}
-
-int Player::getHealth()
-{
-	return health;
 }
 
 Sprite Player::getSprite()
@@ -71,32 +45,68 @@ void Player::stopUp()
 {
 	upPressed = false;
 }
-
 void Player::stopDown()
 {
 	downPressed = false;
+}
+
+FloatRect Player::getPosition()
+{
+	return playerSprite.getGlobalBounds();
 }
 
 void Player::update(float elapsedTime, Vector2i mousePosition)
 {
 	if (upPressed)
 	{
-		mousePosition.y -= speed * elapsedTime;
+		playerPosition.y -= speed * elapsedTime;
 	}
 
 	if (downPressed)
 	{
-		mousePosition.y += speed * elapsedTime;
+		playerPosition.y += speed * elapsedTime;
 	}
 
 	if (rightPressed)
 	{
-		mousePosition.x += speed * elapsedTime;
+		playerPosition.x += speed * elapsedTime;
 	}
 
 	if (leftPressed)
 	{
-		mousePosition.x -= speed * elapsedTime;
+		playerPosition.x -= speed * elapsedTime;
 	}
-	
+
+	playerSprite.setPosition(playerPosition);
+
+
+
+	// Keep the player in the arena
+	/*if (m_Position.x > m_Arena.width - m_TileSize)
+	{
+		m_Position.x = m_Arena.width - m_TileSize;
+	}
+
+	if (m_Position.x < m_Arena.left + m_TileSize)
+	{
+		m_Position.x = m_Arena.left + m_TileSize;
+	}
+
+	if (m_Position.y > m_Arena.height - m_TileSize)
+	{
+		m_Position.y = m_Arena.height - m_TileSize;
+	}
+
+	if (m_Position.y < m_Arena.top + m_TileSize)
+	{
+		m_Position.y = m_Arena.top + m_TileSize;
+	}
+
+	// Calculate the angle the player is facing
+	float angle = (atan2(mousePosition.y - m_Resolution.y / 2,
+		mousePosition.x - m_Resolution.x / 2)
+		* 180) / 3.141;
+
+	m_Sprite.setRotation(angle);
+	*/
 }
