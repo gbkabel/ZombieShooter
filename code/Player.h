8a65a8210836c2f5f0c26744cb1a282d1b5a0dd8
gbkabel <112282802+gbkabel@users.noreply.github.com>
2022-12-06 17:45:17 -0800

    Update Player.h

--- code/Player.h
+++ code/Player.h
@@ -5,11 +5,9 @@
 class Player : public Character
 {
 private:
-
 	const int HIT_INVINCIBILITY_TIME_MS = 200;
 	const int STARTING_HEALTH = 100;
 	const int STARTING_SPEED = 100;
-
 	bool upPressed;
 	bool downPressed;
 	bool leftPressed;
@@ -17,14 +15,13 @@ private:
 	Vector2f playerInputVector = {0, 0};
 	Time m_LastHit;
 	void DetermineMoveDirection();
-	
 public:
 	Player(int, int, int, std::string);
 	void Spawn(int, int) override;
 	void Heal(int);
 	bool Hit(Time, int);
-	Time GetLastHitTime();
-	int GetHealth();
+	Time GetLastHitTime() const;
+	int GetHealth() const;
 	void MoveLeft();
 	void MoveRight();
 	void MoveUp();
