722ac62e71bda7e7559960736ed21d240f5b9c9f
gbkabel <112282802+gbkabel@users.noreply.github.com>
2022-12-06 17:40:33 -0800

    Update NormalZombie.cpp

--- code/NormalZombie.cpp
+++ code/NormalZombie.cpp
@@ -1,7 +1,6 @@
 #include "NormalZombie.h"
 
-NormalZombie::NormalZombie(int StartX, int StartY, std::string _path) 
-: Zombie(StartX, StartY, NORMAL_HEALTH, _path)
+NormalZombie::NormalZombie(int StartX, int StartY, string _path) : Zombie(StartX, StartY, NORMAL_HEALTH, _path)
 {
     m_Speed = NORMAL_SPEED;
     m_Damage = NORMAL_DAMAGE;
