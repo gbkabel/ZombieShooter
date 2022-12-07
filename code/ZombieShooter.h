eca795b9911f89adc7550c8aa9e48f944e5b2421
gbkabel <112282802+gbkabel@users.noreply.github.com>
2022-12-06 17:56:31 -0800

    Update ZombieShooter.h

--- code/ZombieShooter.h
+++ code/ZombieShooter.h
@@ -1,7 +1,10 @@
-#pragma once
+#ifndef ZOMBIESHOOTER_H
+#define ZOMBIESHOOTER_H
 
 #include <SFML/Graphics.hpp>
 
 using namespace sf;
 
 int CreateBackground(VertexArray& rVA, IntRect arena);
+
+#endif
