664a7e1c95bbec5a16b7b00509098ce027fbf260
gbkabel <112282802+gbkabel@users.noreply.github.com>
2022-12-06 17:24:13 -0800

    Update GameManager.h

--- code/GameManager.h
+++ code/GameManager.h
@@ -11,6 +11,22 @@ using namespace std;
 
 class GameManager
 {
+private:
+    const int NUM_ZOMBIE_TYPES = 2;
+    const int NUM_STARTING_ZOMBIES = 15;
+    const size_t MAX_ZOMBIES_ON_SCREEN = 100;
+    const int SPAWN_COOLDOWN_AMOUNT = 200;
+    Time lastSpawn;
+    Time lastPressed;
+	float fireRate = 1;
+	int currentBullet = 0;
+    int m_Score = 0;
+    Zombie* GetARandomZombieType() const;
+    void SpawnNewZombie();
+    vector<Zombie*> m_Zombies;
+    vector<Bullet*> m_Bullets;
+    vector<Pickup*> m_Pickups;
+    Vector2f m_ArenaSize;
 public:
     GameManager(Vector2f);
     void ShootBullets(Player*, Vector2f, Time);
@@ -24,26 +40,5 @@ public:
     vector<Zombie*> GetZombies() const;
     vector<Bullet*> GetBullets() const;
     vector<Pickup*> GetPickups() const;
-    
     enum class ZombieTypes {NORMAL, CRAWLER, TANK, SPRINTER};
-
-private:
-    const int NUM_ZOMBIE_TYPES = 2;
-    const int NUM_STARTING_ZOMBIES = 15;
-    const size_t MAX_ZOMBIES_ON_SCREEN = 100;
-
-    const int SPAWN_COOLDOWN_AMOUNT = 200;
-    
-    Time lastSpawn;
-    Time lastPressed;
-
-	float fireRate = 1;
-	int currentBullet = 0;
-    int m_Score = 0;
-    Zombie* GetARandomZombieType() const;
-    void SpawnNewZombie();
-    vector<Zombie*> m_Zombies;
-    vector<Bullet*> m_Bullets;
-    vector<Pickup*> m_Pickups;
-    Vector2f m_ArenaSize;
 };
